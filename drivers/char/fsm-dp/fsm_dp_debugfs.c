1f239c9d3fb855e8733ebc2c22b7493e3d84f5ba
Ning Wan <nwan@codeaurora.org>
2021-06-23 06:21:24 -0400

    fsm_dp: resync UL buffer pool ring producer pointer
    Random kill of applications, may cause UL buffer pool ring producer tail
    pointer and head pointer to become out of sync. Take recovery action when
    this condition occurs, otherwise it may stay out of sync forever.
    
    Change-Id: Ie0bf4a9742828ca7f30061cccf251f5c8ea3a300
    CRs-Fixed: 2958126
    Acked-by: Che-Min Hsieh <cheminh@qti.qualcomm.com>
    Signed-off-by: Ning Wan <nwan@codeaurora.org>

--- drivers/char/fsm-dp/fsm_dp_debugfs.c
+++ drivers/char/fsm-dp/fsm_dp_debugfs.c
@@ -625,6 +625,8 @@ static int debugfs_mhi_show(struct seq_file *s, void *unused)
 		   mhi->stats.rx_replenish_err);
 	seq_printf(s, "    RX_OUTOFBUF_DROP:   %lu\n",
 		   mhi->stats.rx_outofbuf_drop);
+	seq_printf(s, "    RX_OUTOFBUF_RESYNC: %lu\n",
+		   mhi->stats.rx_resync);
 	return 0;
 }
 DEFINE_DEBUGFS_OPS(debugfs_mhi, debugfs_mhi_show, NULL);

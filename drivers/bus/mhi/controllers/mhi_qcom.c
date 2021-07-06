c49db39a958c179d030db88223d9c5deb9fe8321
Upal Kumar Saha <usaha@codeaurora.org>
2021-07-06 16:28:01 +0530

    bus: mhi: update firmware table to support latest FSM
    Change-Id: I374fd73f9c1fecc38ada508eb7038900cfa937b4
    CRs-Fixed: 2917893
    Signed-off-by: Upal Kumar Saha <usaha@codeaurora.org>

--- drivers/bus/mhi/controllers/mhi_qcom.c
+++ drivers/bus/mhi/controllers/mhi_qcom.c
@@ -38,6 +38,7 @@ struct firmware_info {
 
 static const struct firmware_info firmware_table[] = {
 	{.dev_id = 0x305, .fw_image = "fsm100xx/sbl1.mbn"},
+	{.dev_id = 0x308, .fw_image = "fsm100xx/xbl.elf"},
 	/* default, set to debug.mbn */
 	{.fw_image = "debug.mbn"},
 };
@@ -690,6 +691,7 @@ static struct pci_device_id mhi_pcie_device_id[] = {
 	{PCI_DEVICE(MHI_PCIE_VENDOR_ID, 0x0304)},
 	{PCI_DEVICE(MHI_PCIE_VENDOR_ID, 0x0305)},
 	{PCI_DEVICE(MHI_PCIE_VENDOR_ID, 0x0306)},
+	{PCI_DEVICE(MHI_PCIE_VENDOR_ID, 0x0308)},
 	{PCI_DEVICE(MHI_PCIE_VENDOR_ID, MHI_PCIE_DEBUG_ID)},
 	{0},
 };
